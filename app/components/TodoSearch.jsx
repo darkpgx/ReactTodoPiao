fe74d0e3912731f470b0c9905bbcaa0b8a007b4d
Guangxian Piao <guangxian.piao@aexp.com>
2017-08-20 15:48:11 -0700

    Test Search component and fix search component bug

--- app/components/TodoSearch.jsx
+++ app/components/TodoSearch.jsx
@@ -11,11 +11,11 @@ var TodoSearch = React.createClass({
     return (
       <div>
         <div>
-          <input type="search" ref="searchText" placeholder="Search todos" onChange="this.handleSearch"/>
+          <input type="search" ref="searchText" placeholder="Search todos" onChange={this.handleSearch}/>
         </div>
         <div>
           <label>
-            <input type="checkbox" ref="showCompleted" onChange="this.handleSearch"/>
+            <input type="checkbox" ref="showCompleted" onChange={this.handleSearch}/>
             Show completed todos
           </label>
         </div>
