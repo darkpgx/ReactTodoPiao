862c50d7de0d261ed55c357193ab955858f1a8fb
Larry Piao <darkpgx@gmail.com>
2017-08-23 01:27:05 -0700

    Style todo application

--- app/components/TodoList.jsx
+++ app/components/TodoList.jsx
@@ -5,6 +5,9 @@ var TodoList = React.createClass({
   render: function() {
     var {todos} = this.props;
     var renderTodos = () => {
+      if(todos.length === 0) {
+        return <p className="container__message">Nothing To Do</p>;
+      }
       //use Array.map to create an array of jsx (in this case array of <Todo/>)
       //important: remember to include key
       return todos.map((todo) => {
