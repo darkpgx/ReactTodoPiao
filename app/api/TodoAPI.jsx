d9fab6eb85685dc7b89cf8edfe1958b71cb5b49b
Larry Piao <darkpgx@gmail.com>
2017-08-22 18:59:51 -0700

    Add filter functionality

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -17,5 +17,34 @@ module.exports = {
     }
 
     return $.isArray(todos) ? todos: [];
+  },
+  filterTodos: function(todos, showCompleted, searchText) {
+    var filteredTodos = todos;
+
+    //Filter by showCompleted
+    //Array.filter: take an existing array and filter based on return of the call back
+    filteredTodos = filteredTodos.filter((todo) => {
+      return !todo.completed || showCompleted;
+    });
+
+    //Filter by searchText
+    filteredTodos = filteredTodos.filter((todo) => {
+      var text = todo.text.toLowerCase();
+      return searchText.length === 0 || text.indexOf(searchText) > -1;
+    });
+
+    //Sort todos with non-completed first
+    //Array.sort: sort the existing array; return -1, a will come before b; return 1, b will come before a; return 0 for default;
+    filteredTodos.sort((a, b) => {
+      if(!a.completed && b.completed) {
+        return -1;
+      } else if (a.completed && !b.completed) {
+        return 1
+      } else {
+        return 0;
+      }
+    });
+
+    return filteredTodos;
   }
 };
